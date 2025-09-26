88e8337dda08d28d9fa0440e620e1863aff9088f
Udayraj Sahu <udayrajsahu098@gmail.com>
2025-09-26 20:17:17 +0530

    authentication and protectedRouting , condititonal Ui Rndering using clerk

---
+++ src/components/navigation.tsx
@@ -0,0 +1,35 @@
+import {
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	SignOutButton,
+	UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+
+export const Navigation = () => {
+	return (
+		<nav className="border-b bg-[var(--background)] border-[var(--foreground)]/10 ">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div className="flex justify-between h-16 items-center">
+					<div className="flex-shrink-0">
+						<h1 className="text-xl font-semibold text-[var(--foreground)]">
+							Next.js App
+						</h1>
+					</div>
+					<div className="flex items-center gap-4">
+						<SignedOut>
+							<SignInButton mode="modal" />
+						</SignedOut>
+						{/* <UserButton /> */}
+						<SignedIn>
+						<Link href="/user-profile">Profile</Link>
+					
+							<SignOutButton />
+						</SignedIn>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
